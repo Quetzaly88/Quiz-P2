c1d1a9ea48ddaf1d560788d4311908a85ec8da60
Andrea Quetzaly Nilsson <andreaycoral@icloud.com>
2024-04-14 21:41:19 +0000

    add some questions to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const questions = [
+    {
+        question: "Where are the oldest mummifications found?",
+        answers: [
+            { text: "Chile", correctAnswer: false },
+            { text: "Egypt", correctAnswer: true }
+        ]
+    },
+    {
+        question: "Who sent Christopher Columbus to explore the New World?",
+        answers: [
+            { text: "Americans", correctAnswer: false },
+            { text: "King Ferdinand", correctAnswer: true }
+        ]
+    }
+];
