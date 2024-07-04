f03fab699b1e5ddc1d718b9fb6becf1b97dfddf4
Andrea Nilsson <andreaycoral@icloud.com>
2024-07-04 17:01:10 +0000

    Add the first variable and an array with the questions for the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+let correctAnswer = 0;
+
+const questions = [
+    {question: "The hummingbird egg is the world's smallest bird egg", correctAnswer: true},
+    {question: "The blue whale is the biggest animal to have ever lived.", correctAnswer: true},
+    {question: "Do camels store water in their humps?", correctAnswer: false},
+    {question: "Is the chemical symbol for gold 'Au'", correctAnswer: true},
+    {question: "The longest river in the world is the Nile", correctAnswer: false}
+]
+
 function initGame () {
 
 }
@@ -15,5 +25,5 @@ function answerFalse () {
 }
 
 function nextQuestion () {
-    
+
 }
